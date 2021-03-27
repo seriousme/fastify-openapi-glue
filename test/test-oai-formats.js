a04c989e4cb6eb4927446f87c515edaeaa02eb25
Hans Klunder <hans.klunder@gmail.com>
2021-03-27 13:19:57 +0100

    Removed dependency on Decimal.js-light

--- test/test-oai-formats.js
+++ test/test-oai-formats.js
@@ -1,8 +1,10 @@
 const t = require("tap");
 const Ajv = require("ajv").default;
+
 // AJV misses some validators for byte, float, double, int32 and int64 that oai-formats adds
 const oaiFormats = require("../lib/oai-formats");
 const test = t.test;
+
 
 test("adding formats to AJV works", t => {
 
@@ -24,7 +26,7 @@ IGNoYXJhY3RlcnMgdG8gdGVzdCBtdWx0aWxpbmUgY2FwYWJpbGl0aWVzCg==`),"multiline base64
 });
 
 test("int32", t => {
-    t.plan(4);
+    t.plan(5);
     const ajv = new Ajv();
     t.throws(() => ajv.compile({ type: 'number', format: 'int32' }), "throws without definition for int32")
     ajv.addFormat('int32', oaiFormats.int32);
@@ -32,10 +34,11 @@ test("int32", t => {
     t.ok(validator(256), "256 ok");
     t.notOk(validator(256.1), "256.1 fails");
     t.notOk(validator(Math.pow(2, 32)), "2^32 fails");
+    t.notOk(validator("a"), "'a' fails");
 });
 
 test("int64", t => {
-    t.plan(4);
+    t.plan(5);
     const ajv = new Ajv();
     t.throws(() => ajv.compile({ type: 'number', format: 'int64' }), "throws without definition for int64")
     ajv.addFormat('int64', oaiFormats.int64);
@@ -43,24 +46,27 @@ test("int64", t => {
     t.ok(validator(256), "256 ok");
     t.notOk(validator(256.1), "256.1 fails");
     t.notOk(validator(Math.pow(2, 64)), "2^64 fails");
+    t.notOk(validator("a"), "'a' fails");
 });
 
 test("float", t => {
-    t.plan(2);
+    t.plan(3);
     const ajv = new Ajv();
     t.throws(() => ajv.compile({ type: 'number', format: 'float' }), "throws without definition for float")
     ajv.addFormat('float', oaiFormats.float);
     const validator = ajv.compile({ type: 'number', format: 'float' });
     t.ok(validator(256.1), "256.1 ok");
+    t.notOk(validator("a"), "'a' fails");
 });
 
 test("double", t => {
-    t.plan(2);
+    t.plan(3);
     const ajv = new Ajv();
     t.throws(() => ajv.compile({ type: 'number', format: 'double' }), "throws without definition for double")
     ajv.addFormat('double', oaiFormats.double);
     const validator = ajv.compile({ type: 'number', format: 'double' });
     t.ok(validator(256.1), "256.1 ok");
+    t.notOk(validator("a"), "'a' fails");
 });
 
 test("binary", t => {
