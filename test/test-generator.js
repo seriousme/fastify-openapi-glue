53dd777a5cbfa5b0c7fe8415cf84b65a63e1381d
Hans Klunder <hans.klunder@gmail.com>
2022-06-26 11:51:05 +0200

    fix: comment on updating checksums

--- test/test-generator.js
+++ test/test-generator.js
@@ -7,12 +7,11 @@ const importJSON = createRequire(import.meta.url);
 const localFile = (fileName) => (new URL(fileName, import.meta.url)).pathname
 const dir = localFile('.');
 
-// if you need new checksums (e.g. because you changed template or swaggerfile)
-// run `node ..\generator.js -c test-swagger.v2.json > test-swagger.v2.checksums.json`
+// if you need new checksums (e.g. because you changed template or spec file)
+// run `npm run updateChecksums`
 const testChecksums = await importJSON("./test-swagger.v2.checksums.json");
 const specPath = localFile("./test-swagger.v2.json");
 
-// run `node ..\generator.js -c test-swagger-noBasePath.v2.json > test-swagger-noBasePath.v2.checksums.json`
 const noBasePathChecksums = await importJSON("./test-swagger-noBasePath.v2.checksums.json");
 const noBasePathSpecPath = await importJSON("./test-swagger-noBasePath.v2.json");
 
