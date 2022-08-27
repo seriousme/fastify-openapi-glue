3c73943f366c43ce0058a4502c90577da3525213
Hans Klunder <hans.klunder@gmail.com>
2022-08-27 09:30:53 +0200

    recursive tests

---
+++ test/test-recursive.v3.js
@@ -0,0 +1,37 @@
+import tap from "tap";
+const test = tap.test;
+import Fastify from "fastify";
+import fastifyOpenapiGlue from "../index.js";
+import { createRequire } from "module";
+const importJSON = createRequire(import.meta.url);
+
+const testSpec = await importJSON("./test-openapi-v3-recursive.json");
+
+import { Service } from "./service.js";
+const service = new Service();
+
+const noStrict = {
+  ajv: {
+    customOptions: {
+      strict: false,
+    },
+  },
+};
+
+test("route registration succeeds", (t) => {
+  const opts = {
+    specification: testSpec,
+    service,
+  };
+
+  t.plan(1);
+  const fastify = Fastify(noStrict);
+  fastify.register(fastifyOpenapiGlue, opts);
+  fastify.ready((err) => {
+    if (err) {
+      t.fail("got unexpected error");
+    } else {
+      t.pass("no unexpected error");
+    }
+  });
+});
