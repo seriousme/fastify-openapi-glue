147e805b4ee5da4596f41d0bd727b70152f11d73
Hans Klunder <hans.klunder@gmail.com>
2026-04-29 18:56:13 +0200

    fix: do not mutate specification parameter

--- test/test-recursive.v3.js
+++ test/test-recursive.v3.js
@@ -19,12 +19,27 @@ const noStrict = {
 	},
 };
 
+function countRefs(value) {
+	if (Array.isArray(value)) {
+		return value.reduce((count, item) => count + countRefs(item), 0);
+	}
+
+	if (value && typeof value === "object") {
+		return Object.entries(value).reduce((count, [key, child]) => {
+			return count + (key === "$ref" ? 1 : 0) + countRefs(child);
+		}, 0);
+	}
+
+	return 0;
+}
+
 test("route registration succeeds with recursion", (t, done) => {
 	const opts = {
 		specification: testSpec,
 		serviceHandlers,
 	};
 
+	const numRefsBefore = countRefs(testSpec);
 	const fastify = Fastify(noStrict);
 	fastify.register(fastifyOpenapiGlue, opts);
 	fastify.ready((err) => {
@@ -32,6 +47,8 @@ test("route registration succeeds with recursion", (t, done) => {
 			t.assert.fail("got unexpected error");
 		} else {
 			t.assert.ok(true, "no unexpected error");
+			const numRefsAfter = countRefs(testSpec);
+			t.assert.equal(numRefsAfter, numRefsBefore, "same number of refs");
 			done();
 		}
 	});
